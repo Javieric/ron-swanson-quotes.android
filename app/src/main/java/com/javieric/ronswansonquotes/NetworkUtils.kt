d9e4f4d67234983bf15f388a382b1a5febd41e03
javi <javier.izaguirre85@gmail.com>
2023-04-05 22:13:58 +0100

    remove deprecated code from NetworkUtils

--- app/src/main/java/com/javieric/ronswansonquotes/NetworkUtils.kt
+++ app/src/main/java/com/javieric/ronswansonquotes/NetworkUtils.kt
@@ -2,14 +2,21 @@ package com.javieric.ronswansonquotes
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import androidx.core.content.getSystemService
 
 object NetworkUtils {
 
     fun checkInternetConnection(context: Context): Boolean {
 
-        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-
-        return cm.activeNetworkInfo != null && cm.activeNetworkInfo!!.isAvailable && cm.activeNetworkInfo!!
-            .isConnected
+        val connectivityManager = context.getSystemService<ConnectivityManager>()
+        connectivityManager?.run {
+            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)?.run {
+                return hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                        hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+                        hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
+            }
+        }
+        return false
     }
 }
